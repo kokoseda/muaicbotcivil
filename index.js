1b9332370b01ff7ca693c0f92e4c09f9a87e3ef0
kokoseda <kokoseda22@yahoo.com>
2018-11-12 22:46:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ client.on('ready', () => {
     console.log(`in ${client.guilds.size} servers `)
     console.log(`[Codes] ${client.users.size}`)
 });
-const prefix = "1"
+const prefix = "A"
 client.on('message', async msg => {
     if (msg.author.bot) return undefined;
     if (!msg.content.startsWith(prefix)) return undefined;
@@ -356,7 +356,7 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== "306464381775118336") return;
+  if (message.author.id !== "314135031029170197") return;
 
   
   if (message.content.startsWith(prefix + 'setwatch')) {
